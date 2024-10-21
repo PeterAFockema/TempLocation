9d685f79b5d655dba16c81a02f50de7b55c93cc8
PeterAFockema <anonymous@person.com>
2024-10-21 21:08:52 +0100

    Python non-repeating character

---
+++ find_non_repeating_characters_in_list.py
@@ -0,0 +1,19 @@
+#find_non_repeating_characters_in_list.py
+
+#Take the user input
+String = "prepinsta"
+
+for i in String:
+    #print(i)
+    #Initialise a count variable
+    count = 0
+    for j in String:
+        #Check for repeated characters
+        if i == j:
+            count += 1
+        #If character is found >1 time, then break the loop
+        if count >1:
+            break
+    #Print for the non-repeating characters
+    if count == 1:
+        print(i, end = " ")
